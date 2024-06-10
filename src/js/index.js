a606ba6e5899beff0117cc11c5b095d5dc5b94c9
Gabriel Fukushima <gabrielfukushima98@gmail.com>
2024-06-10 19:02:47 +0900

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,33 @@
-console.log(document.getElementById("btn-avancar"));
-
 const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
 let cartaoAtual = 0;
 const cartoes = document.querySelectorAll(".cartao");
-const btnVoltar = document.getElementById("btn-voltar")
 
 btnAvancar.addEventListener("click", function () {
+	const ehUltimoCartao = cartaoAtual === cartoes.length - 1;
+	if (ehUltimoCartao) return;
 
-    const ehUltimoCartao = cartaoAtual === cartoes.length - 1;
-    if (ehUltimoCartao) return;
+	esconderCartaoSelecionado();
 
-    esconderCartoeSelecionado();
-
-    cartaoAtual++;
-    mostraCartao();
-
+	cartaoAtual++;
+	mostrarCartao();
 });
 
 btnVoltar.addEventListener("click", function () {
-    const ehPrimeiroCartao = cartaoAtual === 0
-    if (ehPrimeiroCartao) return;
+	const ehPrimeiroCartao = cartaoAtual === 0;
+	if (ehPrimeiroCartao) return;
 
-    esconderCartoeSelecionado();
+	esconderCartaoSelecionado();
 
-    cartaoAtual--;
-    mostraCartao();
-
+	cartaoAtual--;
+	mostrarCartao();
 });
 
-function mostraCartao() {
-    cartoes[cartaoAtual].classList.add("selecionado");
+function mostrarCartao() {
+	cartoes[cartaoAtual].classList.add("selecionado");
 }
 
-function esconderCartoeSelecionado() {
-    const cartaoSelecionado = document.querySelector(".selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
+function esconderCartaoSelecionado() {
+	const cartaoSelecionado = document.querySelector(".selecionado");
+	cartaoSelecionado.classList.remove("selecionado");
 }
